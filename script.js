264c2762fed848c74de3626cf1cc6d2198123724
Your Name <you@example.com>
2023-09-14 22:18:54 +0200

    mariosCompartmen

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const mariosCompartmen = 4;
+const  = 4;
 /*Mario's compartment does not change*/
 const preditionofFather = Number(prompt("Insert Predition"));
 /*father's prediction is changing.*/
@@ -7,9 +7,9 @@ const text2 = "Your son is in a compartment with a larger number.";
 const text3 = "Your son is in a compartment with a smaller number.";
 /*According to your father's guess, what is written on the console changes*/
 if (preditionofFather == mariosCompartmen) {
-  console.log("out1=>", text1);
+  console.log(text1);
 } else if (preditionofFather < mariosCompartmen) {
-  console.log("out2=>", text2);
+  console.log(text2);
 } else {
-  console.log("out3=>", text3);
+  console.log(text3);
 }
